b2e5a0cfab308888a04445dbdfc943c931ffdb4a
CYLI310 <chengyangli3041@gmail.com>
2025-11-19 19:17:26 +0800

    Initial Commit

---
+++ DriveAgentiOS/DriveAgentiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAgentiOSApp.swift
+//  DriveAgentiOS
+//
+//  Created by Justin Li on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct DriveAgentiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
